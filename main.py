4835319ed72e4f1c69d6625a7ea7ed0d5ec7c2ec
marekkomp <195009940+marekkomp@users.noreply.github.com>
2025-01-15 12:09:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ conn = sqlite3.connect("produkty.db")
 df = pd.read_sql_query("SELECT * FROM produkty", conn)
 
 # Tytuł aplikacji
-st.title("Interaktywne filtrowanie produktów kompre.pl")
+st.title("Stan magazynowy kompre.pl")
 
 # Filtry w układzie wielu kolumn
 st.header("Filtry")
